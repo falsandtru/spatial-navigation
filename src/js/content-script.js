de337115b64f2937491d6657f21cfbfd8dfffbe9
falsandtru <falsandtru>
2015-06-17 21:52:45 +0900

    Modify build config

--- src/js/content-script.js
+++
@@ -1,9 +0,0 @@
-function injectScript(filepath, query) {
-  var parent = document.querySelector(query),
-      script = document.createElement('script');
-  script.setAttribute('type', 'text/javascript');
-  script.setAttribute('src', filepath);
-  return parent.appendChild(script);
-};
-
-injectScript(chrome.extension.getURL('/dist/raw/spatialnavigation.js'), 'body');
